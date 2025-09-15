e6321e5c81b8269b4b855f67bb48d8de3b90c5ad
Boki Todorcic <boki.todorcic@softwire.com>
2025-09-15 15:24:24 +0100

    Fixed process error with dotenv

--- backend/fetchArrivals.ts
+++ backend/fetchArrivals.ts
@@ -1,18 +1,16 @@
 import axios from "axios";
-import "dotenv/config";
 
-const api_key: string = process.env.API_KEY;
-const stopID: string = "490008660N"
+const api_key: string | undefined = import.meta.env.API_KEY;
+const stopID: string = "490008660N";
 
-async function getBusArrivals() {
+export async function getBusArrivals() {
   try {
     const response = await axios.get(
-      `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`
+      `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals?api_key=${api_key}`
     );
-    console.log(response);
+    return JSON.stringify(response.data);
   } catch (error) {
     console.error(error);
+    return "No data";
   }
 }
-
-getBusArrivals();
