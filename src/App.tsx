a3299f469e967a167b48b35fa251416f9f3ac336
Boki Todorcic <boki.todorcic@softwire.com>
2025-09-15 15:26:03 +0100

    Added fetchArrivals data to client side

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import { getBusArrivals } from "../backend/fetchArrivals";
+
 function App() {
+  const [arrivalsData, setArrivalsData] = useState<string>();
+
+  async function handleClick() {
+    const arrivalData: string = await getBusArrivals();
+    setArrivalsData(arrivalData);
+  }
+
   return (
-        <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4"
-        >BusBoard</h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4">
+        BusBoard
+      </h1>
+      <button onClick={() => handleClick()}>Click Me!</button>
+      <div>{arrivalsData}</div>
+    </>
+  );
 }
 
-export default App
+export default App;
