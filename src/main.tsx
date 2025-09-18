61758ad42d9061dfeb87a293f481924e00540e58
Boki Todorcic <boki.todorcic@softwire.com>
2025-09-18 16:15:18 +0100

    grunt: added API check on startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const api_key: string | undefined = import.meta.env.VITE_API_KEY;
+
+if (typeof api_key === "undefined") {
+  throw new Error("Cannot accept API key of type 'undefined'");
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
